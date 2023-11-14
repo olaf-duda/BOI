845420d9fd2a305780f1d6c732226b3e7e116f8f
Bartkosa <bart.kosieradzki@gmail.com>
2023-11-14 18:33:46 +0100

    added OSM

--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,31 +1,71 @@
-import { StyleSheet } from 'react-native';
-
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
+import React, { useRef, useCallback } from 'react';
+import { View, TouchableOpacity, Text, StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+import html_script from '../html_script.js';
 
 export default function TabTwoScreen() {
+  const mapRef = useRef<WebView | null>(null);
+
+  const goToMyPosition = (lat: number, lon: number) => {
+    if (mapRef.current) {
+      const script = `
+        if (typeof map !== 'undefined') {
+          map.setView([${lat}, ${lon}], 10);
+          L.marker([${lat}, ${lon}]).addTo(map);
+        }
+      `;
+      mapRef.current.injectJavaScript(script);
+    }
+  };
+  
+
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Jebac szatana</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
-    </View>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView style={styles.container}>
+        <WebView ref={mapRef} source={{ html: html_script }} style={styles.webview} />
+        <View style={styles.buttonArea}>
+          <TouchableOpacity style={styles.button} onPress={() => goToMyPosition(44.7866, 20.4489)}>
+            <Text style={styles.buttonText}>Belgrade</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => goToMyPosition(35.6804, 139.7690)}>
+            <Text style={styles.buttonText}>Tokyo</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={() => goToMyPosition(40.4168, -3.7038)}>
+            <Text style={styles.buttonText}>Madrid</Text>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 10,
+    backgroundColor: 'grey',
+  },
+  webview: {
+    flex: 2,
+  },
+  buttonArea: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
     alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
+  button: {
+    width: 80,
+    padding: 10,
+    borderRadius: 10,
+    backgroundColor: 'black',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: 'white',
     fontWeight: 'bold',
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
+    fontSize: 14,
   },
 });
---
+++ app/html_script.js
@@ -0,0 +1,86 @@
+const html_script = `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<base target="_top">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Quick Start - Leaflet</title>
+	
+	<link rel="shortcut icon" type="image/x-icon" href="docs/images/favicon.ico" />
+
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
+    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo=" crossorigin=""></script>
+
+	<style>
+		html, body {
+			height: 100%;
+			margin: 0;
+		}
+		.leaflet-container {
+			height: 400px;
+			width: 600px;
+			max-width: 100%;
+			max-height: 100%;
+		}
+	</style>
+
+	
+</head>
+<body>
+
+
+
+<div id="map" style="width: 600px; height: 400px;"></div>
+<script>
+
+	const map = L.map('map').setView([52.2297, 21.0122], 13);
+
+	const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		maxZoom: 19,
+		attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+	}).addTo(map);
+
+	const marker = L.marker([51.5, -0.09]).addTo(map)
+		.bindPopup('<b>Hello world!</b><br />I am a popup.').openPopup();
+
+	const circle = L.circle([51.508, -0.11], {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5,
+		radius: 500
+	}).addTo(map).bindPopup('I am a circle.');
+
+	const polygon = L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	]).addTo(map).bindPopup('I am a polygon.');
+
+
+	const popup = L.popup()
+		.setLatLng([52.2297, 21.0122])
+		.setContent('I am a standalone popup.')
+		.openOn(map);
+
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(map);
+	}
+
+	map.on('click', onMapClick);
+
+</script>
+
+
+
+</body>
+</html>
+
+`
+
+export default html_script
