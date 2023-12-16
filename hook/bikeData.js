8becdd64d66570b7d78e4fcd0992b0a160150024
olasconan <olasconan1@tlen.pl>
2023-12-16 16:27:24 +0100

    changed bikeData.js to read from a file not from API

--- hook/bikeData.js
+++ hook/bikeData.js
@@ -1,9 +1,14 @@
 import { useState, useEffect, View } from "react";
 import axios from 'axios';
 import { FlatList } from "react-native-gesture-handler";
+import RNFS from 'react-native-fs';
 
-const apiKey = 'xEsFhYZR5jpO1Ug1';
-const apiUrl = `https://api-gateway.nextbike.pl/api/maps/service/vw/locations`;
+const filePath = '../API_data/archiwum.lst'; // Adjust the path accordingly
+
+
+// We for now read from a local file instead of API
+// const apiKey = 'xEsFhYZR5jpO1Ug1';
+// const apiUrl = `https://api-gateway.nextbike.pl/api/maps/service/vw/locations`;
 
 const useBikeStationList =  () => {
     const [bikeStations, setBikeStations] = useState([]);
@@ -14,17 +19,27 @@ const useBikeStationList =  () => {
 
     const fetchData = async () => {
         try {
-            const response = await axios.get(apiUrl, {
-                headers: {
-                    'Api-Key': apiKey,
-                },
-            });
-            setBikeStations(response.data[0].cities[0].places);
-            console.log(bikeStations.length)
+            const fileContent = await RNFS.readfile(filePath, 'utf8');
+            const data = JSON.parse(fileContent);
+
+            console.log(data[0]);
+            setBikeStations(data[0].cities[0].places);
+
             setIsLoading(false);
+
+            // We for now read from a local file instead of API
+            // const response = await axios.get(apiUrl, {
+            //     headers: {
+            //         'Api-Key': apiKey,
+            //     },
+            // });
+            // setBikeStations(response.data[0].cities[0].places);
+            // console.log(bikeStations.length)
+            // setIsLoading(false);
         } catch (error) {
             console.error('Error:', error);
             setIsLoading(false);
+            setError(error);
         }
     }
 
