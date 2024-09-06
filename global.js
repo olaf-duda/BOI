7bd8d451affbd3cd0eab452094f295d9101b3bb3
Bartkosa <bart.kosieradzki@gmail.com>
2024-09-06 21:43:28 +0200

    First version of apk build

--- global.js
+++ global.js
@@ -1,4 +1,3 @@
 
-//Change the following URL's and IP's for your cases:
-global.ngrokUrl = `https://a6ad-2a02-a310-823d-9980-7c7e-ae15-3bc6-c58d.ngrok-free.app`;
-global.otpApiUrl = 'http://192.168.0.217:8082/otp/routers/default/index/graphql';
+//global.ngrokUrl = `https://e9fc-2a02-a31a-c1fa-2f00-acf7-3955-d2c3-4df6.ngrok-free.app`;
+global.otpApiUrl = 'https://4917-2a02-a31a-c1fa-2f00-9d9d-d4fc-ba11-61b6.ngrok-free.app/otp/routers/default/index/graphql';
--- hook/bikeData.js
+++ hook/bikeData.js
@@ -1,37 +1,35 @@
 import { useState, useEffect, View } from "react";
 import axios from 'axios';
 import { FlatList } from "react-native-gesture-handler";
-import '../global.js'
-//import RNFS from 'react-native-fs';
 
-//const filePath = '../API_data/archiwum.lst'; // Adjust the path accordingly
-
-
-// We for now read from a local file instead of API
+const apiKey = 'xEsFhYZR5jpO1Ug1';
+const apiUrl = `https://api-gateway.nextbike.pl/api/maps/service/vw/locations`;
 
 const useBikeStationList =  () => {
     const [bikeStations, setBikeStations] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
+    console.log("downloading data from API");
 
     const fetchData = async () => {
         try {
-            const response = await axios.get(global.ngrokUrl);
+            const response = await axios.get(apiUrl, {
+                headers: {
+                    'Api-Key': apiKey,
+                },
+            });
             setBikeStations(response.data[0].cities[0].places);
-            console.log("number of bikes", bikeStations.length)
+            console.log(bikeStations.length)
             setIsLoading(false);
         } catch (error) {
             console.error('Error:', error);
             setIsLoading(false);
-            setError(error);
         }
     }
 
     useEffect(() => {
         fetchData();
     }, []);
-
     return {bikeStations, isLoading, error, fetchData};
 };
-
 export default useBikeStationList;
